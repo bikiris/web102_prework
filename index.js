4a49ff4622552d56dca6b151349149d9156773b4
bikiris <a664347635@gmail.com>
2023-11-17 21:17:45 -0500

    Complete challenge 5

--- index.js
+++ index.js
@@ -106,10 +106,12 @@ function filterUnfundedOnly() {
     deleteChildElements(gamesContainer);
 
     // use filter() to get a list of games that have not yet met their goal
-
+    const gamesNotMet = GAMES_JSON.filter((game) => {
+        return game.goal > game.pledged;
+    })
 
     // use the function we previously created to add the unfunded games to the DOM
-
+    addGamesToPage(gamesNotMet);
 }
 
 // show only games that are fully funded
@@ -117,10 +119,12 @@ function filterFundedOnly() {
     deleteChildElements(gamesContainer);
 
     // use filter() to get a list of games that have met or exceeded their goal
-
+    
 
     // use the function we previously created to add unfunded games to the DOM
-
+    addGamesToPage(GAMES_JSON.filter((game) => {
+        return game.pledged >= game.goal;
+    }))
 }
 
 // show all games
@@ -128,7 +132,7 @@ function showAllGames() {
     deleteChildElements(gamesContainer);
 
     // add all games from the JSON data to the DOM
-
+    addGamesToPage(GAMES_JSON);
 }
 
 // select each button in the "Our Games" section
@@ -137,7 +141,9 @@ const fundedBtn = document.getElementById("funded-btn");
 const allBtn = document.getElementById("all-btn");
 
 // add event listeners with the correct functions to each button
-
+unfundedBtn.addEventListener('click',filterUnfundedOnly);
+fundedBtn.addEventListener('click',filterFundedOnly);
+allBtn.addEventListener('click',showAllGames);
 
 /*************************************************************************************
  * Challenge 6: Add more information at the top of the page about the company.
